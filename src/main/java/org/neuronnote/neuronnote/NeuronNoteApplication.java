fd3dff9c1f1ac27168183b104951b3f227e74b11
humaizaffath <mohammedaffath17@gmail.com>
2025-06-27 23:11:33 +0530

    init command

---
+++ src/main/java/org/neuronnote/neuronnote/NeuronNoteApplication.java
@@ -0,0 +1,13 @@
+package org.neuronnote.neuronnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuronNoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuronNoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/neuronnote/neuronnote/NeuronNoteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.neuronnote.neuronnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuronNoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
